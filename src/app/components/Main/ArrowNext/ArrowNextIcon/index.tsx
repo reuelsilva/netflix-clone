d4bca414b7127210033446fbea9d0442e5eae6cc
reuelsilva <reuelsilva19@gmail.com>
2024-06-07 12:22:57 -0300

    chore: Renomeando componente

--- src/app/components/Main/ArrowNext/ArrowNextIcon/index.tsx
+++ src/app/components/Main/ArrowNext/ArrowNextIcon/index.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 
-export default function ArrowForwardIcon():ReactElement{
+export default function ArrowNextIcon():ReactElement{
     return(
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none">
             <path d="M10.5 15L13.5 12L10.5 9" stroke="#FFF"/>
--- src/app/components/Main/ArrowNext/index.tsx
+++ src/app/components/Main/ArrowNext/index.tsx
@@ -1,14 +1,14 @@
 import { MouseEvent, ReactElement } from "react";
-import ArrowForwardIcon from "./ArrowForwardIcon";
+import ArrowNextIcon from "./ArrowNextIcon";
 
-interface ArrowForwardProps{
+interface ArrowNextProps{
     onClick: (event: MouseEvent<HTMLButtonElement>) => void,
 }
 
-export default function ArrowForward({onClick}: ArrowForwardProps):ReactElement{
+export default function ArrowNext({onClick}: ArrowNextProps):ReactElement{
     return(
         <button className="w-12 h-49.5 absolute right-0 bottom-0 z-2 bg-black-100 opacity-100 sm:opacity-0" onClick={onClick}>
-            <ArrowForwardIcon/>
+            <ArrowNextIcon/>
         </button>
     )
 }
